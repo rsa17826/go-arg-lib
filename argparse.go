8828d2a42fa109cda1f26063db0bd7370135999f
nyix <rsa17826@email.vccs.edu>
2026-05-13 18:02:09 -0400

    NO MESSAGE SET

--- argparse.go
+++ argparse.go
@@ -175,7 +175,7 @@ func PrintHelp(defs []ArgumentData, filters []string) {
 
 	// Headers
 	fmt.Printf("  %-*s %-*s %s\n", col1Width, "FLAGS", col2Width, "EXPECTS", "DESCRIPTION")
-	fmt.Printf("  %s%s\n%s", Gray, strings.Repeat("-", col1Width+col2Width+13), Reset)
+	fmt.Printf("  %s%s\n%s", Gray, strings.Repeat("-", col1Width+col2Width+13+20), Reset)
 
 	for _, def := range defs {
 		// 1. Build Key String (with colors)
