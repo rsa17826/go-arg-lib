0f99b5d91bc3885b3611d77e0885cd510fb8fdb0
nyix <rsa17826@email.vccs.edu>
2026-05-13 17:45:14 -0400

    NO MESSAGE SET

--- argparse.go
+++ argparse.go
@@ -280,9 +280,9 @@ func formatExample(def ArgumentData) string {
 			default_ = def.Default[index]
 		}
 		if default_ != nil {
-			return Yellow + "<" + name + "=" + Blue + repr(default_) + Yellow + ">" + Reset
+			return Yellow + "<" + Cyan + name + Yellow + "=" + Blue + repr(default_) + Yellow + ">" + Reset
 		}
-		return Yellow + "<" + name + ">" + Reset
+		return Yellow + "<" + Cyan + name + Yellow + ">" + Reset
 	}
 
 	if def.VarArgs {
