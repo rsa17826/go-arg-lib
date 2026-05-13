c44dde9293fb6c7f927db74789569326e89c02c8
nyix <rsa17826@email.vccs.edu>
2026-05-13 11:27:31 -0400

    NO MESSAGE SET

--- argparse.go
+++ argparse.go
@@ -230,7 +230,7 @@ func PrintHelp(defs []ArgumentData, filters []string) {
 
 		// 5. Example Row
 		if def.AllowDupes || def.VarArgs || def.AfterCount > 1 {
-			fmt.Printf("    %sExample:\n%s%s\n", Gray, formatExample(def), Reset)
+			fmt.Printf("    %sExample:\n    %s%s\n", Gray, formatExample(def), Reset)
 		}
 	}
 }
